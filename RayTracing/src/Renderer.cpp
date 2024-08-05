d01eb1f738fedeb92fe466140ce2dd6016e43bc4
Mason Luna <mason.luna24@yahoo.com>
2024-08-04 21:06:47 -0500

    Rendered a circle :)

--- RayTracing/src/Renderer.cpp
+++ RayTracing/src/Renderer.cpp
@@ -1,6 +1,19 @@
 #include "Renderer.h"
 #include <Walnut/Random.h>
 
+namespace Utils {
+	static uint32_t ConvertToRGBA(const glm::vec4& color)
+	{
+		uint8_t r = (uint8_t)(color.r * 255.0f);
+		uint8_t g = (uint8_t)(color.g * 255.0f);
+		uint8_t b = (uint8_t)(color.b * 255.0f);
+		uint8_t a = (uint8_t)(color.a * 255.0f);
+
+		uint32_t result = (a << 24) | (b << 16) | (g << 8) | r;
+		return result;
+	}
+}
+
 void Renderer::OnResize(uint32_t width, uint32_t height)
 {
 	if (m_FinalImage)
@@ -31,7 +44,11 @@ void Renderer::Render()
 		for (uint32_t x = 0; x < m_FinalImage->GetWidth(); x++) 
 		{
 			glm::vec2 coord = { (float)x / (float)m_FinalImage->GetWidth(), (float)y / m_FinalImage->GetHeight() };
-			m_ImageData[x + y * m_FinalImage->GetWidth()] = PerPixel(coord);
+			coord = coord * 2.0f - 1.0f;
+
+			glm::vec4 color = PerPixel(coord);
+			color = glm::clamp(color, glm::vec4(0.0f), glm::vec4(1.0f));
+			m_ImageData[x + y * m_FinalImage->GetWidth()] = Utils::ConvertToRGBA(color);
 		}
 	}
 
@@ -39,9 +56,36 @@ void Renderer::Render()
 
 }
 
-uint32_t Renderer::PerPixel(glm::vec2 coord)
+// (bx^2 + by^2)t^2 + 2(axbx + ayby)t + (ax^2 + ay^2 - r^2) = 0
+// a = ray origin
+// b = ray direction
+// r = radius
+// t = hit distance
+
+glm::vec4 Renderer::PerPixel(glm::vec2 coord)
 {
-	uint8_t r = (uint8_t)(coord.x * 255.0f);
-	uint8_t g = (uint8_t)(coord.y * 255.0f);
-	return 0xff000000 | (g << 8) | r;
+
+	glm::vec3 rayOrigin(0.0f, 0.0f, 2.0f);
+	glm::vec3 rayDirection(coord.x, coord.y, -1.0f);
+	float radius = 0.5f;
+
+	float a = glm::dot(rayDirection, rayDirection);
+	float b = 2.0f * glm::dot(rayOrigin, rayDirection);
+	float c = glm::dot(rayOrigin, rayOrigin) - radius * radius;
+
+	float discriminant = b * b - 4.0f * a * c;
+
+	if (discriminant <= 0.0f)
+		return glm::vec4(0, 0, 0, 1);
+
+	glm::vec3 sphereColor(1, 0, 1);
+
+	float t0 = (-b + glm::sqrt(discriminant)) / (2 * a);
+	float closestT = (-b - glm::sqrt(discriminant)) / (2 * a);
+
+	glm::vec3 hitPoint = rayOrigin + rayDirection * closestT;
+
+
+
+	return glm::vec4(hitPoint, 1);
 }
