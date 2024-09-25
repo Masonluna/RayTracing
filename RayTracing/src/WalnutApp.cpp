ef9f267300cfbc22463a1f1931258a9dbdbd401e
Mason Luna <mason.luna24@yahoo.com>
2024-09-25 15:23:31 -0500

    Implemented the Material class

--- RayTracing/src/WalnutApp.cpp
+++ RayTracing/src/WalnutApp.cpp
@@ -14,11 +14,19 @@ class ExampleLayer : public Walnut::Layer
 public:
 	ExampleLayer() : m_Camera(45.0, 0.1f, 100.0f)
 	{
+		Material& whiteSphere = m_Scene.Materials.emplace_back();
+		whiteSphere.Albedo = { 1.0f, 1.0f, 1.0f };
+		whiteSphere.Roughness = 0.0f;
+
+		Material& blueSphere = m_Scene.Materials.emplace_back();
+		blueSphere.Albedo = { 0.2f, 0.3f, 1.0f };
+		blueSphere.Roughness = 0.1f;
+
 		{
 			Sphere sphere;
 			sphere.Position = { 0.0f, 0.0f, 0.0f };
 			sphere.Radius = 0.5f;
-			sphere.Albedo = { 1.0f, 1.0f, 1.0f };
+			sphere.MaterialIndex = 0;
 			m_Scene.Spheres.push_back(sphere);
 		}
 
@@ -26,7 +34,7 @@ public:
 			Sphere sphere;
 			sphere.Position = { 1.0f, 0.0f, -5.0f };
 			sphere.Radius = 1.5f;
-			sphere.Albedo = { 0.2f, 0.3f, 1.0f };
+			sphere.MaterialIndex = 1;
 			m_Scene.Spheres.push_back(sphere);
 		}
 	}
@@ -48,15 +56,32 @@ public:
 
 		for (int i = 0; i < m_Scene.Spheres.size(); i++) {
 			ImGui::PushID(i);
+			Sphere& sphere = m_Scene.Spheres[i];
 
-			ImGui::DragFloat3("Position", glm::value_ptr(m_Scene.Spheres[i].Position), 0.1f);
-			ImGui::DragFloat("Radius", &m_Scene.Spheres[i].Radius, 0.1f);
-			ImGui::ColorEdit3("Albedo", glm::value_ptr(m_Scene.Spheres[i].Albedo));
+			ImGui::DragFloat3("Position", glm::value_ptr(sphere.Position), 0.1f);
+			ImGui::DragFloat("Radius", &sphere.Radius, 0.1f);
+			ImGui::DragInt("Material", &sphere.MaterialIndex, 1.0f, 0, (int)m_Scene.Materials.size() - 1);
+
+
 
 			ImGui::Separator();
 
 			ImGui::PopID();
 		}
+
+		for (size_t i = 0; i < m_Scene.Materials.size(); i++) {
+			ImGui::PushID(i);
+
+			Material& material = m_Scene.Materials[i];
+			ImGui::ColorEdit3("Albedo", glm::value_ptr(material.Albedo));
+			ImGui::DragFloat("Roughness", &material.Roughness, 0.05f, 0.0f, 1.0f);
+			ImGui::DragFloat("Metallic", &material.Metallic, 0.05f, 0.0f, 1.0f);
+
+			ImGui::Separator();
+
+			ImGui::PopID();
+		}
+
 		ImGui::End();
 
 		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
